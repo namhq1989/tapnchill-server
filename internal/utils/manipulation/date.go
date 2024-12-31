04b03a67d5e31a56e49dfe0f536c7e21192a28fe
Nam Huynh <namhq1989@users.noreply.github.com>
2024-12-31 14:12:21 +0700

    fix(server): change timezone functions (#65)

--- internal/utils/manipulation/date.go
+++ internal/utils/manipulation/date.go
@@ -5,11 +5,32 @@ import (
 	"time"
 )
 
-func getLocation(tz string) *time.Location {
+func resolveTimezone(tz string) *time.Location {
 	if tz == "" {
 		return time.UTC
 	}
 
+	// Handle fixed offsets like "+07:00"
+	if len(tz) == 6 && (tz[0] == '+' || tz[0] == '-') {
+		var sign int
+		if tz[0] == '+' {
+			sign = 1
+		} else {
+			sign = -1
+		}
+
+		var hours, minutes int
+		_, err := fmt.Sscanf(tz[1:], "%02d:%02d", &hours, &minutes)
+		if err != nil {
+			fmt.Printf("invalid offset format '%s', defaulting to UTC: %v\n", tz, err)
+			return time.UTC
+		}
+
+		offsetSeconds := sign * (hours*3600 + minutes*60)
+		return time.FixedZone(tz, offsetSeconds)
+	}
+
+	// Handle named time zones
 	loc, err := time.LoadLocation(tz)
 	if err != nil {
 		fmt.Printf("invalid timezone '%s', defaulting to UTC: %v\n", tz, err)
@@ -18,13 +39,18 @@ func getLocation(tz string) *time.Location {
 	return loc
 }
 
-func GetTimezoneIdentifier(t time.Time) string {
-	location := t.Location()
-	return location.String()
+// func GetTimezoneIdentifier(t time.Time) string {
+// 	location := t.Location()
+// 	return location.String()
+// }
+
+func GetUTCOffset(t time.Time) string {
+	_, offset := t.Zone()
+	return fmt.Sprintf("%+03d:%02d", offset/3600, (offset%3600)/60)
 }
 
 func Now(tz string) time.Time {
-	loc := getLocation(tz)
+	loc := resolveTimezone(tz)
 	return time.Now().In(loc)
 }
 
@@ -34,7 +60,7 @@ func NowUTC() time.Time {
 
 func IsToday(t time.Time, tz string) bool {
 	now := Now(tz)
-	tInLoc := t.In(getLocation(tz))
+	tInLoc := t.In(resolveTimezone(tz))
 
 	return tInLoc.Year() == now.Year() && tInLoc.Month() == now.Month() && tInLoc.Day() == now.Day()
 }
@@ -42,7 +68,7 @@ func IsToday(t time.Time, tz string) bool {
 func IsYesterday(t time.Time, tz string) bool {
 	now := Now(tz)
 	yesterday := now.AddDate(0, 0, -1)
-	tInLoc := t.In(getLocation(tz))
+	tInLoc := t.In(resolveTimezone(tz))
 
 	return tInLoc.Year() == yesterday.Year() && tInLoc.Month() == yesterday.Month() && tInLoc.Day() == yesterday.Day()
 }
@@ -76,13 +102,13 @@ func GetEndOfDayWithClientDate(date string) (*time.Time, error) {
 }
 
 func PreviousDay(date time.Time, tz string) time.Time {
-	loc := getLocation(tz)
+	loc := resolveTimezone(tz)
 	yesterday := date.AddDate(0, 0, -1)
 	return yesterday.In(loc)
 }
 
 func IsSameDay(date1 time.Time, date2 time.Time, tz string) bool {
-	loc := getLocation(tz)
+	loc := resolveTimezone(tz)
 	date1InLoc := date1.In(loc)
 	date2InLoc := date2.In(loc)
 	return date1InLoc.Year() == date2InLoc.Year() && date1InLoc.Month() == date2InLoc.Month() && date1InLoc.Day() == date2InLoc.Day()
